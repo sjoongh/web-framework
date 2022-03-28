a35ca68f90fc4a92dba2d1841916d89d5296c8a4
sjoongh <az45687@naver.com>
2022-03-28 11:49:44 +0900

    npm bootstrap

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 // main.js
+import bootstrap from './node_modules/bootstrap'
 
 const emailInputEl = document.querySelector('#exampleInputEmail1');
 const modalEl = document.querySelector('#exampleModal');
 
+// modalEl.addEventListener('shown.bs.modal', function() {
+//     emailInputEl.foucs();
+// }) 
+
 modalEl.addEventListener('shown.bs.modal', function() {
     emailInputEl.foucs();
-}) 
+})
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
