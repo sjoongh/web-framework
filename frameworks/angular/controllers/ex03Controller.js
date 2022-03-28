7c46df8d262ca1b5925db933d66fb881026ef139
sjoongh <az45687@naver.com>
2022-03-28 15:58:41 +0900

    angular.js

--- frameworks/angular/controllers/ex03Controller.js
+++ frameworks/angular/controllers/ex03Controller.js
@@ -1,8 +1,9 @@
 // ex03Controller.js
 
-var myApp = angular.module('app', []);
+// var myApp = angular.module('app', []);
 // controller("컨트롤러이름", function($scope) { })
-myApp
+// myApp
+angular.module('app', [])
     .controller("ex03Controller", function($scope) {
         $scope.controllerName = "app.ex03Controller";
     })
