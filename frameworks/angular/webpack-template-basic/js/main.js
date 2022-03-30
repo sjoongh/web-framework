b772270f6f65e36ca27ce4af49d11b681d5c9538
sjoongh <az45687@naver.com>
2022-03-30 11:55:23 +0900

    webpack with style&sass-loader

--- frameworks/angular/webpack-template-basic/js/main.js
+++ frameworks/angular/webpack-template-basic/js/main.js
@@ -1,1 +1,3 @@
+import '../css/main.scss' // css적용만을 위해 이름 적용x
+
 console.log("webpack");
--- frameworks/angular/webpack-template-basic/webpack.config.js
+++ frameworks/angular/webpack-template-basic/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin') // 특정한 디렉터리나 파일을 복사해 번들링 된 폴더 내에 경로로 삽입하는 플러그인
 // export
 module.exports = {
     // 파일을 읽어들이기 시작하는 진입점 설정
@@ -17,9 +18,35 @@ module.exports = {
         filename: 'main.js',
         clean: true, // 이전의 build파일 clean
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+            test: /\.s?css$/, // css 파일로 만들고 싶을 때 style-loader대신 사용한다 
+            use: [ // 뒤(sass-loader)에서부터 실행된다 && 모듈로 변경된 css를 js로 읽게함
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ],
+            }
+        ]
+    },
     // 번들링 후 결과물의 처리 방식
     plugins: [
-        new HtmlWebpackPlugin({template: './index.html'})
-    ]
+        new HtmlWebpackPlugin({template: './index.html'}),
+        new CopyPlugin({patterns: [
+            { from: 'static' }
+        ]})
+    ],
+
+    devServer: {
+        host: 'localhost'
+    }
 }
 // import
