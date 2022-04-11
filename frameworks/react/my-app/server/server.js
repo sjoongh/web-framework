95d277f76eb0ed7fbf80581a4b4fa9ab4f4ab8e2
sjoongh <az45687@naver.com>
2022-04-11 14:44:33 +0900

    node.js

--- frameworks/react/my-app/server/server.js
+++ frameworks/react/my-app/server/server.js
@@ -7,6 +7,14 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'public/main.html'));
 });
 
+app.get('/beauty', function(request, response) {
+    response.send('beauty page');
+});
+
+app.get('/game', function(request, response) {
+    response.send('game page');
+});
+
 const http = require('http').createServer(app);
 http.listen(8080, function() {
     console.log('server on start 8080');
